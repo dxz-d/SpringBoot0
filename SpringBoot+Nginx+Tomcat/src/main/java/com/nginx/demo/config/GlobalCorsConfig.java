0cd2e1652833639fd0bdeac65b49a84195667c40
dxz-d <1205760997@qq.com>
2021-06-08 16:26:08 +0800

    SpringBoot+Nginx+Tomcat

---
+++ SpringBoot+Nginx+Tomcat/src/main/java/com/nginx/demo/config/GlobalCorsConfig.java
@@ -0,0 +1,43 @@
+package com.nginx.demo.config;
+
+/**
+ * @description:
+ * @author: dxz
+ * @date: 2021/6/7 15:00
+ */
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class GlobalCorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        //1.添加CORS配置信息
+        CorsConfiguration config = new CorsConfiguration();
+        //1) 允许的域,不要写*，否则cookie就无法使用了
+        // config.addAllowedOrigin("http://8.140.115.178");
+        config.addAllowedOrigin("http://localhost");
+        //2) 是否发送Cookie信息
+        config.setAllowCredentials(true);
+        //3) 允许的请求方式
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        // 4）允许的头信息
+        config.addAllowedHeader("*");
+
+        //2.添加映射路径，我们拦截一切请求
+        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+        configSource.registerCorsConfiguration("/**", config);
+
+        //3.返回新的CorsFilter.
+        return new CorsFilter(configSource);
+    }
+}
