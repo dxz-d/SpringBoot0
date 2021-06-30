91ed33eb3874416793d7a45d221c409d82ed7a7f
dxz-d <1205760997@qq.com>
2021-06-30 11:35:15 +0800

    1

--- SpringBoot+Restful/src/test/java/com/example/demo/DemoApplicationTests.java
+++ SpringBoot+Restful/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,33 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 @SpringBootTest
 class DemoApplicationTests {
 
     @Test
     void contextLoads() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date today = new Date();
+        // 当前日期
+        String endDate = sdf.format(today);
+        Calendar theCa = Calendar.getInstance();
+        theCa.setTime(today);
+
+        // 一月之后日期
+        theCa.add(theCa.MONTH, 1);
+        Date start = theCa.getTime();
+        String startDate = sdf.format(start);
+
+        // 一年之后日期
+        theCa.add(Calendar.YEAR, 1);
+        Date date = theCa.getTime();
+        String aYearAgo = sdf.format(date);
+
+        System.out.println("当前日期：" +endDate+ ",30天之后的日期:" +startDate+ ",一年之后的日期：" +aYearAgo);
     }
 
 }
