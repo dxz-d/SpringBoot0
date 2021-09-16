09fdcade75018b1da1c0fd4fccd56e8d58af3e42
dxz-d <1205760997@qq.com>
2021-09-16 15:56:35 +0800

    来自:作者 https://blog.csdn.net/qq_37949192/article/details/103962848

--- SpringBoot+MQTT/src/main/java/com/mqtt/Application.java
+++ SpringBoot+MQTT/src/main/java/com/mqtt/Application.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 来源:https://blog.csdn.net/qq_37949192/article/details/103962848
 
 @MapperScan(basePackages = {"com.mqtt.mapper"})
 @SpringBootApplication
